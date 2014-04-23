e89e3541b3129be917401394773f3a76e7066c2c
baconkwan <terencest@gmail.com>
2014-04-23 16:29:38 +0800

    Apr 23, 2014 16:29

--- exercise/T9_3.pl
+++ exercise/T9_3.pl
@@ -2,11 +2,11 @@
 use utf8;
 use strict;
 
-my $what1 = <STDIN>;
-my $what2 = <STDIN>;
-chomp $what1;
-chomp $what2;
 $^I = "~";
-while (<>){
+while(<>){
+	chomp;
+	s/Fred/\n/g;
+	s/Wilma/Fred/g;
+	s/\n/Fred/g;
 	print;
 }
